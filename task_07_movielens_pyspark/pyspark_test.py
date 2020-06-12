b6a3d9c0d628e77278d24b1d3625298c44117165
data <r.mansel@perceivedata.com>
2020-06-12 14:33:29 +0100

    Pyspark code improvements.

--- task_07_movielens_pyspark/pyspark_test.py
+++ task_07_movielens_pyspark/pyspark_test.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
+#################################################################
+#################################################################
+# You must supply the path to the ml-100k folder as an argument #
+#################################################################
+#################################################################
 
 from pyspark import SparkContext
 from pyspark import SparkConf
@@ -7,28 +12,30 @@ from datetime import datetime
 from pyspark.sql import SQLContext
 from itertools import chain
 import pyspark.sql.functions as F
+from sys import argv
+from io import open # workaround for open() encoding bug
 
 
 #This function returns a movie names dict.
-def loadMovieNames():
+def loadMovieNames(dir_prefix):
     movie_names = {}
-    with open("./ml-100k/u.item",encoding = "ISO-8859-1") as f:
+    with open(dir_prefix + "/u.item",encoding = "ISO-8859-1") as f:
         for line in f: # for each line
             fields = line.split('|') #turn line into list of items
             movie_names[int(fields[0])] = fields[1] #adds name to dict with movie id as the name's key.
     return movie_names
 
 #This function returns a genres dict.
-def loadGenres():
+def loadGenres(dir_prefix):
     genre_names = {}
-    with open("./ml-100k/u.genre",encoding = "ISO-8859-1") as f:
+    with open(dir_prefix + "/u.genre",encoding = "ISO-8859-1") as f:
         for line in f: # for each line
             fields = line.split('|') #turn line into list of items
             if len(fields) == 2:
                 genre_names[int(fields[1])] = fields[0] #adds genre to dict with genre id as the key.
     return genre_names
 
-#function to parse files according to provided schema
+#function to parse files according to provided type list
 def parseFile(line, types, selection, sep=None):
     fields = line.split(sep)
     result = []
@@ -48,12 +55,12 @@ def parseFile(line, types, selection, sep=None):
                 try:   
                     result.append(datetime.fromtimestamp(int(item)))
                 except:
-                    result.append(datetime.min) #keeps dtype consistency and is easy to filter. Null also an option but might cause exceptions.              
+                    result.append(datetime.min) #keeps dtype consistency when data is missing and is easy to filter out.               
             elif types[idx] == 'date':
                 try:
                     result.append(datetime.strptime(item, '%d-%b-%Y')) #Assumes certain format. Could use inference but inefficient.
                 except:
-                    result.append(datetime.min) #keeps dtype consistency and is easy to filter. Null also an option but might cause exceptions.         
+                    result.append(datetime.min) #keeps dtype consistency when data is missing and is easy to filter out.       
             else:
                 result.append(item)  
     return result
@@ -80,9 +87,10 @@ def main():
     sq = SQLContext(sc)
 
     # Load necessary files
-    data_f = sc.textFile("./ml-100k/u.data")
-    user_f = sc.textFile("./ml-100k/u.user")
-    item_f = sc.textFile("./ml-100k/u.item")
+    dir_prefix = argv[1]
+    data_f = sc.textFile(dir_prefix + "/u.data")
+    user_f = sc.textFile(dir_prefix +"/u.user")
+    item_f = sc.textFile(dir_prefix +"/u.item")
     
     # Define types
     data_t = ['int','int','float','timestamp']
@@ -90,18 +98,19 @@ def main():
     item_t = ['int','str', 'date', 'date', 'str', 'int', 'int', 'int', 'int', 'int', 'int', 'int', 'int', 'int', 'int', 'int'
               , 'int', 'int', 'int', 'int', 'int', 'int', 'int', 'int']
     
-    # Create dicts, lists and alias maps
-    movie_names = loadMovieNames()
+    # Create required dicts, lists and alias maps
+    movie_names = loadMovieNames(dir_prefix)
     name_map = F.create_map([F.lit(x) for x in chain(*movie_names.items())])
-    genre_names = loadGenres()
+    genre_names = loadGenres(dir_prefix)
     genre_cols = list(genre_names.values())
+    genre_cols.insert(0,'release_date')
     genre_cols.insert(0,'movie_id')
     genre_range = list(range(5,24))
+    genre_range.insert(0,2)   
     genre_range.insert(0,0)    
         
     # Parse files
-    item_date = sq.createDataFrame(item_f.map(lambda f: parseFile(f, item_t, [0,2],'|')),['movie_id','release_date'])
-    item_genre = sq.createDataFrame(item_f.map(lambda f: parseFile(f, item_t, genre_range,'|')),genre_cols)
+    item_g = sq.createDataFrame(item_f.map(lambda f: parseFile(f, item_t, genre_range,'|')),genre_cols)
     data_g = sq.createDataFrame(data_f.map(lambda f: parseFile(f, data_t, [0,1,2,3])),['user_id','movie_id','rating','timestamp'])    
     user_g = sq.createDataFrame(user_f.map(lambda f: parseFile(f, user_t, [0,1,3,4],'|')),["user_id", "age", "occupation", "zip_code"])
         
@@ -116,19 +125,19 @@ def main():
     
     # Q3.Print list of the number of ratings by genre  
     print("\nQuestion 3:")
-    data_mid_genre = data_g.select('movie_id').join(item_genre, ['movie_id'], how='full')
+    data_mid_genre = data_g.select('movie_id').join(item_g, ['movie_id'], how='full')
     data_mid_genre.select(data_mid_genre.columns[1:]).groupBy().sum().show(truncate=False)
     
     # Q4.Print oldest movie with a 5 rating.
     print("\nQuestion 4:")
-    data_five = data_g.select('movie_id','rating').filter(data_g.rating == 5)   
-    data_mid_date = data_five.join(item_date, ['movie_id'], how='full')
+    data_mid_date = data_g.select('movie_id','rating').filter(data_g.rating == 5)\
+    .join(item_g.select('movie_id','release_date'), ['movie_id'], how='full')
     data_mid_date.filter(data_mid_date.release_date != datetime.min)\
     .orderBy('release_date').limit(1).select(name_map[data_mid_date['movie_id']].alias('movie_name')).show(truncate=False)
     
     # Q5.Print a list of the genre of the top 10 most rated movies. 
     print("\nQuestion 5:")
-    top_ten_genres = top_ten_ratings.join(item_genre, ['movie_id'],how='left')
+    top_ten_genres = top_ten_ratings.join(item_g, ['movie_id'],how='left')
     top_ten_genres_l = top_ten_genres.rdd.map(lambda x: genreReplace(x.asDict(), movie_names))
     for item in top_ten_genres_l.take(10):
         print(item)
@@ -139,7 +148,7 @@ def main():
     user_g_data_g.select('movie_id','user_id').filter(user_g_data_g.occupation == 'student').groupBy('movie_id').count()\
     .orderBy('count', ascending=False).limit(1).select(name_map[user_g_data_g['movie_id']].alias('movie_name')).show(truncate=False)
     
-    # Q7.Print the list of movies that received the highest number of “5” rating 
+    # Q7.Print the list of movies that received the highest number of '5' rating 
     print("\nQuestion 7:")
     data_g.select('movie_id','rating').filter(data_g.rating == 5).groupBy('movie_id').count().orderBy('count', ascending=False).limit(10)\
     .select(name_map[data_g['movie_id']].alias('movie_name')).show(truncate=False)
@@ -156,7 +165,7 @@ def main():
     # Q10.Print the list of movies that were rated after year 1960. Is this question correct? Should it be "made" after 1960?
     print("\nQuestion 10:")
     data_g.select('movie_id','timestamp').filter(data_g.timestamp >= datetime(1960, 1, 1))\
-    .select(name_map[data_g['movie_id']].alias('movie_name')).show(truncate=False)
+    .limit(10).select(name_map[data_g['movie_id']].alias('movie_name')).show(truncate=False)
     
     # Bonus. Convert UnixTimeStamp in timestamp column of u.data to human readable date 
     print("\nBonus Question:")

bb0ad35c8e64cd842a1b65e3371e9b537a6786dd
datadev <r.mansel@perceivedata.com>
2020-06-13 14:37:16 +0100

    Fix for pip selenium bug.

--- task_05_env_installation_scripts/scripts/JDK_get_python_reqs.sh
+++ task_05_env_installation_scripts/scripts/JDK_get_python_reqs.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+#Fix for pip ssl bug.
+python2 -m pip install --trusted-host pypi.python.org --trusted-host files.pythonhosted.org --trusted-host pypi.org --upgrade pip
+
 #Install Selenium
-pip install selenium 2>> $HOME/Software/log.txt
+python2 -m pip install selenium 2>> $HOME/Software/log.txt
 
 #Call JDK Downloader 
-python $3/JDK_get.py $1 $2 2>> $HOME/Software/log.txt
+python2 $3/JDK_get.py $1 $2 2>> $HOME/Software/log.txt
 bash $3/java_install.sh 2>> $HOME/Software/log.txt
 
 #cleanup

e8edfff9f6e68ad006ed099b2da1db551866f4f1
data <r.mansel@perceivedata.com>
2020-07-21 10:30:10 +0100

    fixed shell script directory references

--- capstone_project/src/main/python/start_stream.sh
+++ capstone_project/src/main/python/start_stream.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-mkdir -p ./logs/kafka_success
-mkdir -p ./logs/kafka_failure
+mkdir -p ../../../logs/kafka_success
+mkdir -p ../../../logs/kafka_failure
 
 consumer(){
-	until python3 ./transcript.py $1; do
+	until python3 ./transcript.py $1 transcripts ../../../ ; do
     		echo "${1} consumer crashed with exit code $?.  Restarting.." >&2
     		sleep 1
 	done
 }
 producer(){
-	until python3 ./input.py $1 $2; do
+	until python3 ./input.py $1 $2 ../../../; do
 		echo "${1} producer crashed with exit code $?. Restarting.." >&2
 		sleep 1
 	done
